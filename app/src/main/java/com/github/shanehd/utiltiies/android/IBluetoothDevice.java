8a6727103ff41d179881f473301c677007ebc6ff
Shane <Shane@Shane-PC>
2016-07-19 13:09:24 +0100

    First commit

---
+++ app/src/main/java/com/github/shanehd/utiltiies/android/IBluetoothDevice.java
@@ -0,0 +1,10 @@
+package com.github.shanehd.utiltiies.android;
+
+/**
+ * Created by Shane on 19/07/2016.
+ * @author https://www.github.com/ShaneHD
+ */
+public interface IBluetoothDevice {
+    String getAddress();
+    String getName();
+}
